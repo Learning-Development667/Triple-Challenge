e4f3b202c66bedd5c9dbdde8778b26c97b09a63e
Learning-Development667 <markbrown667@gmail.com>
2026-06-01 19:16:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
 //  Your JSONBin credentials live here permanently.
 // ============================================================
 
-const BIN_ID  = 'YOUR_BIN_ID_HERE';
-const API_KEY = 'YOUR_API_KEY_HERE';
+const BIN_ID  = '6a1c638621f9ee59d2a1ac2c';
+const API_KEY = '$2a$10$3ndQ23/GtXtiDjSQ8iUDOOQ7Qi/0m2u8KKkGmhfz5tJv1hSfb/U2W';
 const BIN_URL = `https://api.jsonbin.io/v3/b/${BIN_ID}`;
